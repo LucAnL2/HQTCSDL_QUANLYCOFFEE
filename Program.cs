28ce761270c5896b3e2879e4618acd024fe40ca4
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2024-11-01 07:51:00 +0700

    reset

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace DemoCSDL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FManager());
+            Application.Run(new FWorker());
         }
     }
 }
